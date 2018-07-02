5a315411e7a57deeefe123380875ea0ab139c372
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-07-02 23:25:18 +0700

    membuat tampilan program dan nama sembako yang dijual

--- src/cobasayembara/sembako.java
+++ src/cobasayembara/sembako.java
@@ -9,5 +9,12 @@ public class sembako {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         int pilih,jml,hrg;
+        
+    System.out.println("=========================");
+    System.out.println("Program Penghitung Jualan Sembako Hanafi");
+    System.out.println("=========================");
+    System.out.println("Menyediakan :");
+    System.out.println("1. Beras\n2. Minyak Goreng\n3. Telur");
+    System.out.println("4. Gula");
     }
 }
